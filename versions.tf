fc1ee93e9df26e92a19de963766a43ab6daead11
Satyam Priyam <satyampriyam32@gmail.com>
2025-09-21 10:17:59 +0100

    chore: terraform fmt

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    aws         = { source = "hashicorp/aws",        version = "~> 5.0" }
-    kubernetes  = { source = "hashicorp/kubernetes", version = "~> 2.33" }
-    helm        = { source = "hashicorp/helm",       version = "~> 2.13" }
+    aws        = { source = "hashicorp/aws", version = "~> 5.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.33" }
+    helm       = { source = "hashicorp/helm", version = "~> 2.13" }
   }
 }
 # Providers will be configured in envs when you deploy for real.
